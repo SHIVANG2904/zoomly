8547e2fe7435f04703e1ce4399aac2c3c3f2b5a3
shivang <shivangagarwal3165@gmail.com>
2025-01-05 20:26:59 +0530

    zoomly

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from '@clerk/nextjs/server';
+ 
+export default authMiddleware()
+ 
+export const config = {
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
+    '/', // Run middleware on index page
+    '/(api|trpc)(.*)'], // Run middleware on API routes
+};
